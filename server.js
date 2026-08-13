0276eb727f715b8f0989fbfb256697466d1fb0bd
gnjoegbu-bit <gnjoegbu@gmail.com>
2026-08-13 23:02:35 +0100

    save

--- server.js
+++ server.js
@@ -32,10 +32,15 @@ function isValidEmail(value) {
 // ========================================
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || ""
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect(err => {
@@ -48,12 +53,12 @@ db.connect(err => {
         );
 
         return;
+
     }
 
     console.log("Connected to MySQL!");
 
 });
-
 
 // ========================================
 // JSON RESPONSE
